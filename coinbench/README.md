bf1087ab4bb161010b4cce6112a480a8abd993f4
nassersaazi <nassersnoop@gmail.com>
2021-06-17 02:48:59 +0300

    Deleted the sass files, edited README

--- coinbench/README.md
+++ coinbench/README.md
@@ -1,2 +1,2 @@
-# EASYBANK
+# COINBENCH
 This project is for applying html,css and javascript fundamentals to create a project about crypto, a topic that I am passionate about 
--- coinbench/src/js/views/searchView.js
+++ coinbench/src/js/views/searchView.js
@@ -1,3 +1,52 @@
 import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value; 
+
+export const clearInput = () => {
+    elements.searchInput.value = "";
+  };
+
+  export const clearResults = () => {
+    elements.searchResList.innerHTML = "";
+    // elements.searchResPages.innerHTML = "";
+  };
+
+const renderArticle = article => {
+    const markup = `
+          <li>
+              <a class="results__link" href="#${article.url}">
+                  <figure class="results__fig">
+                      <img src="${article.urlToImage}" alt="${article.title}">
+                  </figure>
+                  <div class="results__data">
+                      <h4 class="results__name">${limitArticleTitle(
+                        article.title
+                      )}</h4>
+                      <p class="results__author">${article.author}</p>
+                  </div>
+              </a>
+          </li>
+      `;
+    elements.searchResList.insertAdjacentHTML("beforeend", markup);
+  };
+  
+
+  export const limitArticleTitle = (title, limit = 20) => {
+    const newTitle = [];
+    if (title.length > limit) {
+      title.split(" ").reduce((acc, cur) => {
+        if (acc + cur.length <= limit) {
+          newTitle.push(cur);
+        }
+        return acc + cur.length;
+      }, 0);
+  
+      // return the result
+      return `${newTitle.join(" ")} ...`;
+    }
+    return title;
+  };
+
+export const renderResults = articles => {
+    articles.forEach(renderArticle);
+}
