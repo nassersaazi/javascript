eb83d0b0a3293fef29ebdf65dae9d7050d3d8c14
nassersaazi <nassersnoop@gmail.com>
2022-02-23 22:11:52 +0300

    Finished hero section version 1

--- react-projects/portfolio/src/components/Hero/Hero.js
+++ react-projects/portfolio/src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi ,I'm Nasser
+      </SectionTitle>
+      <SectionText>
+      I am a software engineer with 3 years experience with frontend, backend and devops skills.<br/>
+      I have previously developed fintech systems. I currently maintain systems for monitoring servers . I also contribute to crypto open source projects in my free time.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://nassersaazi.github.io/resume.html'}>View Resume</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
